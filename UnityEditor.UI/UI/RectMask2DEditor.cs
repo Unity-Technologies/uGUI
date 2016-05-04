135781d917b69d1cea6b8cae439bfb23dfbe0b8d
Philip Cosgrave <philip@unity3d.com>
2016-05-04 09:26:32 -0400

    Adding Missing file from last 5.4b16 push..

---
+++ UnityEditor.UI/UI/RectMask2DEditor.cs
@@ -0,0 +1,13 @@
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    [CustomEditor(typeof (RectMask2D), true)]
+    [CanEditMultipleObjects]
+    public class RectMask2DEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+        }
+    }
+}
